c6511a0a5c7191d4ae0fc3608f953a837971f8bb
Eric Holk <ericholk@microsoft.com>
2023-07-19 14:20:05 -0700

    Add cancellable async/await macros

--- src/cancelable.rs
+++ src/cancelable.rs
@@ -14,7 +14,7 @@ pub trait Future {
         // By default we preserve the existing cancellation behavior, which is that
         // we don't do anything and we let synchronous destructors do the cleanup.
         Poll::Ready(())
-    };
+    }
 }
 
 /// A compatibility shim to let us use standard Rust futures as cancellable futures
@@ -61,7 +61,7 @@ macro_rules! async_cancel {
     ($body:block) => {
         $crate::cancelable::future_from_generator(
             #[allow(unreachable_code)]
-            |poll_state: $crate::cancelable::PollState| {
+            static move |poll_state: $crate::cancelable::PollState| {
                 unsafe {
                     $crate::cancelable::save_poll_state(poll_state);
                 }
@@ -75,8 +75,8 @@ macro_rules! async_cancel {
 
 #[derive(Clone, Copy)]
 pub struct PollState {
-    cx: NonNull<Context<'static>>,
-    is_cancelled: bool,
+    pub cx: NonNull<Context<'static>>,
+    pub is_cancelled: bool,
 }
 
 static mut POLL_STATE: PollState = PollState {
@@ -92,35 +92,49 @@ pub unsafe fn get_poll_state() -> PollState {
     POLL_STATE
 }
 
+/// A version of await that handles cancellation
 macro_rules! awaitc {
-    ($f:expr) => {
+    ($f:expr) => {{
         let mut f = core::pin::pin!($f);
         loop {
-            let $crate::cancelable::PollState { cx, is_cancelled } = unsafe {
-                $crate::cancelable::get_poll_state()
-            };
+            let $crate::cancelable::PollState { cx, is_cancelled } =
+                unsafe { $crate::cancelable::get_poll_state() };
 
-            let cx: &mut Context<'_> = &mut *cx.as_ptr();
+            let cx: &mut core::task::Context<'_> = unsafe { &mut *cx.as_ptr() };
 
-
+            if is_cancelled {
+                match f.as_mut().poll_cancel(cx) {
+                    core::task::Poll::Ready(()) => {
+                        return $crate::cancelable::CancelState::Cancelled
+                    }
+                    _ => {}
+                }
+            } else {
+                match f.as_mut().poll(cx) {
+                    core::task::Poll::Ready(v) => {
+                        break v;
+                    }
+                    _ => {}
+                }
+            }
 
             unsafe {
                 $crate::cancelable::save_poll_state(yield);
             }
         }
-    };
+    }};
 }
 
 pub fn ready<T>(t: T) -> impl Future<Output = T> {
-    struct Ready<T>(T);
+    struct Ready<T>(Option<T>);
 
     impl<T> Future for Ready<T> {
         type Output = T;
 
         fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-            Poll::Ready(self.0)
+            unsafe { Poll::Ready(self.get_unchecked_mut().0.take().unwrap()) }
         }
     }
 
-    Ready(t)
+    Ready(Some(t))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 #![feature(generators, generator_trait)]
 
+use cancelable::{ready, Future};
+
 #[macro_use]
 mod cancelable;
 mod iocp;
 
 fn main() {
     let fut = async_cancel!({
-        println!("Hello, world!");
+        let world = awaitc!(ready("world"));
+        println!("Hello, {world}!");
     });
-
 }
