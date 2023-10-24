365b8f41bf39c96343197f72a920af62424a9f9a
Eric Holk <ericholk@microsoft.com>
2023-10-24 11:47:57 -0700

    Update example to show cancellation in cancellation

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,14 @@
     let_chains
 )]
 
+use std::{cell::RefCell, task::Poll};
+
 use cancelable::{ready, Future};
 
-use crate::cancelable::{pending, FutureExt};
+use crate::{
+    cancelable::{pending, poll_fn, FutureExt},
+    executor::Executor,
+};
 
 #[macro_use]
 mod cancelable;
@@ -16,14 +21,34 @@ mod executor;
 mod iocp;
 
 fn main() {
-    let fut = async_cancel!({
-        let world = awaitc!(ready("world"));
-        println!("Hello, {world}!");
-        awaitc!(pending()
-            .on_cancel(async_cancel!({ println!("cancelled") }))
-            .race(async_cancel!({ 42 })))
-    });
+    let done = &RefCell::new(false);
+    let root_task = async_cancel!({
+        let a = async_cancel!({ 42 });
+        let mut cancel_started = false;
+        let b = pending().on_cancel(poll_fn(|_| {
+            if !cancel_started {
+                println!("begin cancelling `b`");
+                cancel_started = true;
+            }
+            if *done.borrow() {
+                println!("cancellation of `b` complete");
+                Poll::Ready(())
+            } else {
+                Poll::Pending
+            }
+        }));
 
-    let result = executor::Executor::new(fut).run();
-    println!("result: {:?}", result);
+        awaitc!(a.race(b).on_cancel(async_cancel!({
+            println!("cancelling `race` future");
+        })));
+    })
+    .on_cancel(async_cancel!({
+        println!("cancelling root future");
+    }));
+
+    let mut executor = Executor::new(root_task);
+    let _ = executor.poll();
+    let _ = executor.poll();
+    let _ = executor.poll();
+    *done.borrow_mut() = true;
 }
